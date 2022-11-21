1c5f98f4d54404995f44170eedcc0a3ed84e5c0f
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2022-11-21 18:44:40 +0300

    balls_collide_handle

--- bill.cpp
+++ bill.cpp
@@ -12,6 +12,7 @@ Description: <empty>
 
 #define BALL_SPEED 2500.0f
 #define BALL_FRICTION 3.0f
+#define BALL_RADIUS 20.0f
 Ball update_ball(Ball *ball, Vec2Dim<F32> acc, F32 dt)
 {
     // NOTE(annad):
@@ -32,14 +33,55 @@ Ball update_ball(Ball *ball, Vec2Dim<F32> acc, F32 dt)
 }
 
 // TODO(annad): Refactoring...
-B32 balls_is_collide(Vec2Dim<F32> *ball_a_pos, 
-                     Vec2Dim<F32> *ball_b_pos, 
-                     F32 radius)
+B32 balls_is_collide(Vec2Dim<F32> *ball_a_pos, Vec2Dim<F32> *ball_b_pos)
 {
     F32 distance = (*ball_a_pos - *ball_b_pos).getLength();
-    return distance < (2.0f * radius);
+    return distance < (2.0f * BALL_RADIUS);
 }
 
+
+void balls_collide_handle(Ball *ball_a, Ball *ball_b, Vec2Dim<F32> ball_a_acc, F32 dt)
+{
+    // TODO(annad): All acceleration params must 
+    // work in function with explicit
+    // setting or pass to func.
+    // NOTE(annad): Recalculate positions in collision.
+    Vec2Dim<F32> delta_pos = ball_b->pos - ball_a->pos;
+    F32 cos_fi = ball_a->vel.innerProduct(delta_pos)
+        / delta_pos.getLength()
+        * ball_a->vel.getLength();
+    F32 v = ball_a->vel.getLength() * cos_fi;
+    F32 s = delta_pos.getLength() - 2.0f * BALL_RADIUS;
+    F32 a = 0.5f * ball_a_acc.getLength();
+    // TODO(annad): math, sqrt
+    F32 discriminant = (F32)sqrt(square(v) - 4 * a * s);
+    F32 t = f32Abs((-v + discriminant) / (2.0f * a));
+    // NOTE(annad): Recalculate velocity after collision.
+    ball_a->pos += (ball_a_acc * 0.5f * square(t) + ball_a->vel * t);
+    ball_a->vel += (ball_a_acc * t);
+    Vec2Dim<F32> direct_a = {
+        delta_pos.x / delta_pos.getLength(),
+        delta_pos.y / delta_pos.getLength()
+    };
+    Vec2Dim<F32> direct_b = {
+        direct_a.y, 
+        -direct_a.x,
+    };
+    F32 cos_direct_a = ball_a->vel.innerProduct(direct_a)
+        / ball_a->vel.getLength()
+        * direct_a.getLength();
+    F32 cos_direct_b = ball_a->vel.innerProduct(direct_b)
+        / ball_a->vel.getLength()
+        * direct_b.getLength();
+    
+    // NOTE(annad): Apply
+    ball_a->vel = direct_a * ball_a->vel.getLength() * cos_direct_a;
+    ball_b->vel += direct_b * ball_a->vel.getLength() * cos_direct_b;
+
+    dt -= t;
+
+    // TODO(annad): Continues calculate or stop?......
+}
 
 void game_update_and_render(GameMemory *game_memory, 
                             Renderer *renderer, 
@@ -125,14 +167,14 @@ void game_update_and_render(GameMemory *game_memory,
         F32 dt = (((F32)game_time->dt / 1000.0f));
         for(S32 i = 0; i < BALL_ENUM_COUNT; i += 1)
         {
-            Ball *ball = &game_state->ball[i];
+            Ball *ball_a = &game_state->ball[i];
             Vec2Dim<F32> ball_acc = {};
             if(i == BALL_ENUM_WHITE) 
             {
                 ball_acc = ball_control_acc; // TODO(annad): Remove this.
             }
-            Ball updated_ball = update_ball(ball, ball_acc, dt);
-            
+
+            Ball updated_ball_a = update_ball(ball_a, ball_acc, dt);
             for(S32 j = 0; j < BALL_ENUM_COUNT; j += 1)
             {
                 if(i == j) 
@@ -141,49 +183,15 @@ void game_update_and_render(GameMemory *game_memory,
                 }
                 
                 Ball *ball_b = &game_state->ball[j];
-                if(balls_is_collide(&updated_ball.pos, &ball_b->pos, (F32)const_ball_radius))
+                if(balls_is_collide(&updated_ball_a.pos, &ball_b->pos))
                 {
-                    EvalPrint(i);
+                    __debugbreak();
+                    balls_collide_handle(ball_a, ball_b, ball_acc, dt);
+                    updated_ball_a = *ball_a;
                 }
             }
             
-            *ball = updated_ball;
-            
-            /*             
-                        if(balls_is_collide(&newPlayerPos, &ball->pos, (F32)const_ball_radius))
-                        {
-                            // IMPORTANT(Saiel): You suck, rewrite this.
-                            // TODO(annad): Use t = (-v + (F32)sqrt(square(v) - (4*a*s))) / (2.0f * a)!!!
-                            // NOTE(annad): Recalc position!
-                            Vec2Dim<F32> S = ball->pos - white_ball->pos;
-                            F32 cos = white_ball->vel.innerProduct(S) / (S.getLength() * white_ball->vel.getLength());
-                            F32 v = white_ball->vel.getLength() * cos;
-                            F32 s = S.getLength() - 2.0f * (F32)const_ball_radius;
-                            F32 a = 0.5f * ball_control_acc.getLength();
-                            F32 D = (F32)sqrt(square(v) - (4*a*(s)));
-                            F32 t = f32Abs((-v + D) / (2.0f * a));
-                            // newPlayerPos = playerPos + (white_ball->vel * t);
-                            newPlayerPos = white_ball->pos + (((ball_control_acc * 0.5f * square(t))) + 
-                                                              (white_ball->vel * t));
-                            newPlayerVel = white_ball->vel + ball_control_acc * t;
-                            // newPlayerVel = white_ball->vel + ball_control_acc * t;
-                            // NOTE(annad): Recalc of Velocity!
-                            Vec2Dim<F32> directV1 = {S.x / S.getLength(), S.y / S.getLength()};
-                            Vec2Dim<F32> directV2 = {directV1.y, -directV1.x};
-                            
-                            DEBUG_ballDirection = directV1;
-                            DEBUG_playerDirection = directV2;
-                            
-                            F32 cos_v1 = newPlayerVel.innerProduct(directV1) / (newPlayerVel.getLength() * directV1.getLength());
-                            F32 v1 = newPlayerVel.getLength() * cos_v1;
-                            
-                            F32 cos_v2 = newPlayerVel.innerProduct(directV2) / (directV2.getLength() * newPlayerVel.getLength());
-                            F32 v2 = newPlayerVel.getLength() * cos_v2;
-                            
-                            ball->vel = directV1 * v1;
-                            newPlayerVel = (directV2) * v2;
-                        }
-                         */
+            *ball_a = updated_ball_a;
         }
     }
     
