6a7e07bee07f67edd13ef8be7f26213ac93c32e2
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2023-04-14 00:28:25 +0600

    extra slow simd bilerp

--- bill_renderer_software.cpp
+++ bill_renderer_software.cpp
@@ -19,60 +19,15 @@ U32 textureGetPixel(HTexture *texture, V2DU32 p)
     
     return out;
 }
-#if 0
-BilinearSample 
-textureGetBilinearSample(HTexture *texture, V2DS32 vcell)
-{
-    BilinearSample sample = textureGetBilinearSample;
-    sample.topLeft = textureGetPixel(texture, vcell);
-    sample.topRight = textureGetPixel(texture, vcell + V2DS32{1, 0});
-    sample.bottomLeft = textureGetPixel(texture, vcell + V2DS32{0, 1});
-    sample.bottomRight = textureGetPixel(texture, vcell + V2DS32{1, 1});
-    return sample;
-}
-#endif
 
-U32 __pxlerp(U32 A, U32 B, U32 t)
+U32 __pxlerp(U32 A, U32 B, F32 t)
 {
-#if 1
-    // 74.000000
-    F32 ft = (F32)t / 255.0f;
-    U8 alpha = (U8)((F32)(A >> 24 & 0xff) * ft + (F32)(B >> 24 & 0xff) * (1.0f - ft));
-    U8 red = (U8)((F32)(A >> 16 & 0xff) * ft + (F32)(B >> 16 & 0xff) * (1.0f - ft));
-    U8 green = (U8)((F32)(A >> 8 & 0xff) * ft + (F32)(B >> 8 & 0xff) * (1.0f - ft));
-    U8 blue = (U8)((F32)(A & 0xff) * ft + (F32)(B & 0xff) * (1.0f - ft));
+    U8 alpha = (U8)((F32)(A >> 24 & 0xff) * t + (F32)(B >> 24 & 0xff) * (1.0f - t));
+    U8 red = (U8)((F32)(A >> 16 & 0xff) * t + (F32)(B >> 16 & 0xff) * (1.0f - t));
+    U8 green = (U8)((F32)(A >> 8 & 0xff) * t + (F32)(B >> 8 & 0xff) * (1.0f - t));
+    U8 blue = (U8)((F32)(A & 0xff) * t + (F32)(B & 0xff) * (1.0f - t));
     return alpha << 24 | red << 16 | green << 8 | blue;
-#else
-    // 87.000000
-    U8 alpha = (U8)(((A >> 24 & 0xff)*(255 - t) + t*(B >> 24 & 0xff))/255);
-    U8 red = (U8)(((A >> 16 & 0xff)*(255 - t) + t*(B >> 16 & 0xff))/255);
-    U8 green = (U8)(((A >> 8 & 0xff)*(255 - t) + t*(B >> 8 & 0xff))/255);
-    U8 blue = (U8)(((A & 0xff)*(255 - t) + t*(B & 0xff))/255);
-    return alpha << 24 | red << 16 | green << 8 | blue;
-#endif
 }
-
-#if 0
-void renderTextureFast(Screen *screen, HTexture *texture, V2DF32 pos, V2DF32 vscale)
-{
-    V2DF32 whTexture = {(F32)texture->w, (F32)texture->h};
-    for (U32 y = 0; y < screen->h; y += 1)
-    {
-        for (U32 x = 0; x < screen->w; x += 1)
-        {
-            V2DF32 UV = {
-                (F32)x / (F32)screen->w,
-                (F32)y / (F32)screen->h
-            };
-            
-            V2DF32 vdenorm = (UV + pos) * vscale * whTexture;
-            U32 out = textureGetPixel(texture, V2DU32{(U32)vdenorm.x, (U32)vdenorm.y});
-
-            screen->buf[y*screen->w+x] = out;
-        }
-    }
-}
-#endif
 
 void renderTextureSlow(Screen *screen, HTexture *texture, V2DF32 pos, V2DF32 vscale)
 {
@@ -89,31 +44,89 @@ void renderTextureSlow(Screen *screen, HTexture *texture, V2DF32 pos, V2DF32 vsc
             V2DF32 vdenorm = (UV + pos) * vscale * whTexture;
             vdenorm -= V2DF32{0.5f, 0.5f};
             V2DU32 vcell = {(U32)f32Floor(vdenorm.x), (U32)f32Floor(vdenorm.y)};
-            V2DU32 voffset = {
-                (U32)((vdenorm.x - (F32)vcell.x) * 255.0f), 
-                (U32)((vdenorm.y - (F32)vcell.y) * 255.0f)
-            };
+            V2DF32 voffset = {(vdenorm.x - (F32)vcell.x), (vdenorm.y - (F32)vcell.y)};
             BilinearSample sample;
             sample.topLeft = textureGetPixel(texture, vcell);
             sample.topRight = textureGetPixel(texture, vcell + V2DU32{1, 0});
             sample.bottomLeft = textureGetPixel(texture, vcell + V2DU32{0, 1});
             sample.bottomRight = textureGetPixel(texture, vcell + V2DU32{1, 1}); 
-            
-
-            // load top left
-            // load top right
-            //
-            // |r|g|b|a|
-            //
-            // load bottom left
-            // load bottom right
-
             U32 topX = __pxlerp(sample.topRight, sample.topLeft, voffset.x);
             U32 botX = __pxlerp(sample.bottomRight, sample.bottomLeft, voffset.x);
             U32 out = __pxlerp(botX, topX, voffset.y);
             screen->buf[y*screen->w+x] = out;
         }
     }
+}
+
+U32 bilerp(BilinearSample *s, V2DF32 voffset)
+{
+    ///////////////////////////////////////////////////////////////////////
+    __m256 A = _mm256_mul_ps(
+        _mm256_set_ps(
+            (F32)(s->A >> 24 & 0xff), 
+            (F32)(s->A >> 16 & 0xff), 
+            (F32)(s->A >> 8 & 0xff), 
+            (F32)(s->A & 0xff), 
+
+            (F32)(s->C >> 24 & 0xff), 
+            (F32)(s->C >> 16 & 0xff), 
+            (F32)(s->C >> 8 & 0xff), 
+            (F32)(s->C & 0xff)
+        ), 
+
+        _mm256_set1_ps(1.0f - voffset.x)
+    );
+
+    __m256 B = _mm256_mul_ps(
+        _mm256_set_ps(
+            (F32)(s->B >> 24 & 0xff), 
+            (F32)(s->B >> 16 & 0xff), 
+            (F32)(s->B >> 8 & 0xff), 
+            (F32)(s->B & 0xff), 
+
+            (F32)(s->D >> 24 & 0xff), 
+            (F32)(s->D >> 16 & 0xff), 
+            (F32)(s->D >> 8 & 0xff), 
+            (F32)(s->D & 0xff)
+        ),
+
+        _mm256_set1_ps(voffset.x)
+    );
+
+    __m256 x = _mm256_add_ps(A, B);
+    A = _mm256_mul_ps(
+        _mm256_set_ps(
+            x.m256_f32[3],
+            x.m256_f32[2],
+            x.m256_f32[1],
+            x.m256_f32[0],
+
+            0.0f, 0.0f, 0.0f, 0.0f
+        ), 
+
+        _mm256_set1_ps(voffset.y)
+    );
+
+    B = _mm256_mul_ps(
+        _mm256_set_ps(
+            x.m256_f32[7],
+            x.m256_f32[6],
+            x.m256_f32[5],
+            x.m256_f32[4],
+
+            0.0f, 0.0f, 0.0f, 0.0f
+        ), 
+
+        _mm256_set1_ps(1.0f - voffset.y)
+    );
+
+    __m256 y = _mm256_add_ps(A, B);
+    __m256i out = _mm256_cvtps_epi32(y);
+    return out.m256i_u32[7] << 24 | 
+        out.m256i_u32[6] << 16 | 
+        out.m256i_u32[5] << 8 | 
+        out.m256i_u32[4];
+
 }
 
 void renderTextureFast(Screen *screen, HTexture *texture, V2DF32 pos, V2DF32 vscale)
@@ -127,22 +140,13 @@ void renderTextureFast(Screen *screen, HTexture *texture, V2DF32 pos, V2DF32 vsc
             V2DF32 vdenorm = (UV + pos) * vscale * whTexture;
             vdenorm -= V2DF32{0.5f, 0.5f};
             V2DU32 vcell = {(U32)f32Floor(vdenorm.x), (U32)f32Floor(vdenorm.y)};
-            V2DU32 voffset = {
-                (U32)((vdenorm.x - (F32)vcell.x) * 255.0f), 
-                (U32)((vdenorm.y - (F32)vcell.y) * 255.0f)
-            };
+            V2DF32 voffset = {(vdenorm.x - (F32)vcell.x), (vdenorm.y - (F32)vcell.y)};
             BilinearSample sample;
             sample.topLeft = textureGetPixel(texture, vcell);
             sample.topRight = textureGetPixel(texture, vcell + V2DU32{1, 0});
             sample.bottomLeft = textureGetPixel(texture, vcell + V2DU32{0, 1});
             sample.bottomRight = textureGetPixel(texture, vcell + V2DU32{1, 1});
-
-            ///////////////////////////////////////////////////////////////////////
-            U32 topX = __pxlerp(sample.topRight, sample.topLeft, voffset.x);
-            U32 botX = __pxlerp(sample.bottomRight, sample.bottomLeft, voffset.x);
-            U32 out = __pxlerp(botX, topX, voffset.y);
-            ///////////////////////////////////////////////////////////////////////
-
+            U32 out = bilerp(&sample, voffset);
             screen->buf[y*screen->w+x] = out;
         }
     }
